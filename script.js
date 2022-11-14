45bde748c1ed08433dec4083b233afc02fe01e11
Luis Zubia <soyzubia@gmail.com>
2022-11-14 15:36:46 -0600

    Add show/close mobile menu feature with javascript.

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let hbutton = document.querySelector('.hamb');
 let mobileMenu = document.querySelector('#mobile_menu');
 let closeMenu = document.querySelector('.topx');
+let headerMenu = document.querySelector('.menu');
 
 hbutton.addEventListener('click',function(event) {
     mobileMenu.classList.replace('hide_menu','show_menu');
+    headerMenu.classList.replace('show_menu','hide_menu');
 });
 
 closeMenu.addEventListener('click',function(event) {
     mobileMenu.classList.replace('show_menu','hide_menu');
+    headerMenu.classList.replace('hide_menu','show_menu');
 })
