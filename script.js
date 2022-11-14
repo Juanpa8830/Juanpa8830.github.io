067b3990220511580bccc890a36c07fcb01f657a
Juanpa8830 <jp3_4@hotmail.com>
2022-11-14 17:30:06 -0500

    fix liners errors at styles.css and script.js

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-let hbutton = document.querySelector('.hamb');
-let mobileMenu = document.querySelector('#mobile_menu');
-let closeMenu = document.querySelector('.topx');
-let headerMenu = document.querySelector('.menu');
-let libutton1 =document.querySelector('.top-a1');
-let libutton2 =document.querySelector('.top-a2');
-let libutton3 =document.querySelector('.top-a3');
-let libutton4 =document.querySelector('.top-a4');
+const hbutton = document.querySelector('.hamb');
+const mobileMenu = document.querySelector('#mobile_menu');
+const closeMenu = document.querySelector('.topx');
+const headerMenu = document.querySelector('.menu');
+const libutton1 = document.querySelector('.top-a1');
+const libutton2 = document.querySelector('.top-a2');
+const libutton3 = document.querySelector('.top-a3');
+const libutton4 = document.querySelector('.top-a4');
 
-hbutton.addEventListener('click',function(event) {
-    mobileMenu.classList.replace('hide_menu','show_menu');
-    headerMenu.classList.replace('show_menu','hide_menu');
+hbutton.addEventListener('click', () => {
+  mobileMenu.classList.replace('hide_menu', 'show_menu');
+  headerMenu.classList.replace('show_menu', 'hide_menu');
 });
 
-closeMenu.addEventListener('click',function(event) {
-    mobileMenu.classList.replace('show_menu','hide_menu');
-    headerMenu.classList.replace('hide_menu','show_menu');
-})
+closeMenu.addEventListener('click', () => {
+  mobileMenu.classList.replace('show_menu', 'hide_menu');
+  headerMenu.classList.replace('hide_menu', 'show_menu');
+});
 
-libutton1.addEventListener('click',function(event) {
-    mobileMenu.classList.replace('show_menu','hide_menu');
-    headerMenu.classList.replace('hide_menu','show_menu');
-})
+libutton1.addEventListener('click', () => {
+  mobileMenu.classList.replace('show_menu', 'hide_menu');
+  headerMenu.classList.replace('hide_menu', 'show_menu');
+});
 
-libutton2.addEventListener('click',function(event) {
-    mobileMenu.classList.replace('show_menu','hide_menu');
-    headerMenu.classList.replace('hide_menu','show_menu');
-})
+libutton2.addEventListener('click', () => {
+  mobileMenu.classList.replace('show_menu', 'hide_menu');
+  headerMenu.classList.replace('hide_menu', 'show_menu');
+});
 
-libutton3.addEventListener('click',function(event) {
-    mobileMenu.classList.replace('show_menu','hide_menu');
-    headerMenu.classList.replace('hide_menu','show_menu');
-})
+libutton3.addEventListener('click', () => {
+  mobileMenu.classList.replace('show_menu', 'hide_menu');
+  headerMenu.classList.replace('hide_menu', 'show_menu');
+});
 
-libutton4.addEventListener('click',function(event) {
-    mobileMenu.classList.replace('show_menu','hide_menu');
-    headerMenu.classList.replace('hide_menu','show_menu');
-})
+libutton4.addEventListener('click', () => {
+  mobileMenu.classList.replace('show_menu', 'hide_menu');
+  headerMenu.classList.replace('hide_menu', 'show_menu');
+});
