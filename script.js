c189a74a811aa932bf1fe9838145114963df764d
Juanpa8830 <jp3_4@hotmail.com>
2022-11-14 17:06:52 -0500

    add classes and show/close feature to menu items

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let hbutton = document.querySelector('.hamb');
 let mobileMenu = document.querySelector('#mobile_menu');
 let closeMenu = document.querySelector('.topx');
 let headerMenu = document.querySelector('.menu');
+let libutton1 =document.querySelector('.top-a1');
+let libutton2 =document.querySelector('.top-a2');
+let libutton3 =document.querySelector('.top-a3');
+let libutton4 =document.querySelector('.top-a4');
 
 hbutton.addEventListener('click',function(event) {
     mobileMenu.classList.replace('hide_menu','show_menu');
@@ -9,6 +13,26 @@ hbutton.addEventListener('click',function(event) {
 });
 
 closeMenu.addEventListener('click',function(event) {
+    mobileMenu.classList.replace('show_menu','hide_menu');
+    headerMenu.classList.replace('hide_menu','show_menu');
+})
+
+libutton1.addEventListener('click',function(event) {
+    mobileMenu.classList.replace('show_menu','hide_menu');
+    headerMenu.classList.replace('hide_menu','show_menu');
+})
+
+libutton2.addEventListener('click',function(event) {
+    mobileMenu.classList.replace('show_menu','hide_menu');
+    headerMenu.classList.replace('hide_menu','show_menu');
+})
+
+libutton3.addEventListener('click',function(event) {
+    mobileMenu.classList.replace('show_menu','hide_menu');
+    headerMenu.classList.replace('hide_menu','show_menu');
+})
+
+libutton4.addEventListener('click',function(event) {
     mobileMenu.classList.replace('show_menu','hide_menu');
     headerMenu.classList.replace('hide_menu','show_menu');
 })
